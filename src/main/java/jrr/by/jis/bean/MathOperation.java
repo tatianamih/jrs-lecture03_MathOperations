21c4b26a6ecb7c481f4e8b20957eb3d51f312dd6
“tatianamih” <tmihalchenko66@gmail.com>
2020-08-21 00:52:36 +0300

    My task about mathematical operations

---
+++ src/main/java/jrr/by/jis/bean/MathOperation.java
@@ -0,0 +1,34 @@
+package jrr.by.jis.bean;
+
+public class MathOperation {
+    private int a;
+    private int b;
+    private double d;
+    private  double c;
+
+    public MathOperation(int i, int i1) {
+    }
+
+    public MathOperation(int a, int b, double d, double c) {
+        this.a = a;
+        this.b = b;
+        this.d = d;
+        this.c = c;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public double getD() {
+        return d;
+    }
+
+    public double getC() {
+        return c;
+    }
+}
