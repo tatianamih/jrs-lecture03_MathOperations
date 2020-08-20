f28a001bc16043017d9620e115936ca24b814741
“tatianamih” <tmihalchenko66@gmail.com>
2020-08-21 01:04:26 +0300

    My task about mathematical operations Service

---
+++ src/main/java/jrr/by/service/MathOperationService.java
@@ -0,0 +1,34 @@
+package jrr.by.service;
+
+
+import jrr.by.jis.bean.MathOperation;
+
+public class MathOperationService {
+
+    public int additionOfIntegers(MathOperation mathOperation) {
+
+        return mathOperation.getA() + mathOperation.getB();
+    }
+
+    public int subtractionOfNumbers(MathOperation mathOperation) {
+        return mathOperation.getA() - mathOperation.getB();
+    }
+
+    public int division(MathOperation mathOperation) {
+
+        return mathOperation.getA() / mathOperation.getB();
+    }
+
+    public int multiplication(MathOperation mathOperation) {
+
+        return mathOperation.getA() * mathOperation.getB();
+    }
+
+    public int remainder(MathOperation mathOperation) {
+
+        return mathOperation.getA() % mathOperation.getB();
+    }
+    public  double additionOfDouble(MathOperation mathOperation) {
+        return mathOperation.getC() + mathOperation.getD();
+    }
+}
